73aa695b406d38b7a2526b4878394af79fe7c267
ValeraFabrice <hobyraobera@gmail.com>
2019-04-29 10:20:51 +0200

    modif mon

--- src/Entity/Project.php
+++ src/Entity/Project.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ProjectRepository")
@@ -19,6 +20,8 @@ class Project
     private $id;
 
     /**
+     * @Assert\NotBlank( message = "Saisissez votre nom" )
+     * @Assert\Length(min = 5)
      * @ORM\Column(type="string", length=255)
      */
     private $name;
